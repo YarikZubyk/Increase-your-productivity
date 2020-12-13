778fa96ef715bd77c337d75cc429fa586de52045
Yaroslav <yarik.zubyk@gmail.com>
2020-12-13 15:42:56 +0200

    section blog update

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,23 @@ $(function () {
 $('.below-header-slider').slick({
   dots: true,
 });
+
+$('.container-slider').slick({
+  dots: true,
+  prevArrow:"<img class='slick-prev slick-arrow' src='img/prev.png'>",
+  nextArrow:"<img class='slick-next slick-arrow' src='img/next.png'>"
+});
+/*
+$(document).ready(function() {
+
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+  });
+  
+});*/
